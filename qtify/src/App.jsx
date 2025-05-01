feb90653710a0dc8eb79ad29206463f3582c7d93
deepika <deepika75013@gmail.com>
2025-05-01 15:45:40 +0530

    ME_BUILDOUT_QTIFY_V2_MODULE_ONE

--- qtify/src/App.jsx
+++ qtify/src/App.jsx
@@ -1,17 +1,60 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-import Navbar from './components/Navbar/Navbar'
-//import viteLogo from '/vite.svg'
-import './App.css'
-import Hero from './components/Hero/Hero';
+import React, { useEffect, useState } from "react";
+import Hero from "./components/Hero/Hero";
+import HomePage from "./pages/HomePage/HomePage";
+import Navbar from "./components/Navbar/Navbar";
+import { StyledEngineProvider } from "@mui/material/styles";
+
+import { Outlet } from "react-router-dom";
+import {
+  fetchFilters,
+  fetchNewAlbums,
+  fetchSongs,
+  fetchTopAlbums,
+} from "./api/api";
 
 function App() {
+  const [data, setData] = useState({});
+
+  // const r = {
+  //   topAlbums: [{}, {}, {}, {}],
+  //    newAlbums: [{}, {}, {}, {}],
+  //    genres: ['rock', 'pop', 'jazz'],
+  //    songs: []
+  // };
+
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevState) => {
+        // Object.assign would also work
+        return { ...prevState, [key]: data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+    generateData("genres", fetchFilters);
+  }, []);
+
+  const { topAlbums = [], newAlbums = [], songs = [], genres = [] } = data;
+
   return (
     <>
-      <Navbar />
-      <Hero/>
+      <StyledEngineProvider injectFirst>
+        <Navbar searchData={[...topAlbums, ...newAlbums]} />
+        <Outlet context={{ data: { topAlbums, newAlbums, songs, genres } }} />
+      </StyledEngineProvider>
     </>
-  )
+  );
 }
+
+// {data: {
+//   topAlbums: [],
+//   newAlbums: [],
+//   genres: [],
+//   songs: []
+// }}
 
 export default App;
